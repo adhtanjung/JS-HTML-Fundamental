46d1bd2b12c5a96590d27c10964735c129d77d7d
adhitanjung <adhtanjung@gmail.com>
2020-11-11 13:09:14 +0700

    add logic

--- logic.js
+++ logic.js
@@ -1,19 +1,19 @@
-function xo(str) {
-	let x = 0;
-	let o = 0;
-	let z = str.toLowerCase();
-	// console.log(z.length);
-	for (let i = 0; i < z.length; i++) {
-		if (z[i] == "x") {
-			x++;
-		} else if (z[i] == "o") {
-			o++;
-		}
-	}
-	return `Huruf x: ${x} | Huruf o: ${o}`;
-}
+// function xo(str) {
+// 	let x = 0;
+// 	let o = 0;
+// 	let z = str.toLowerCase();
+// 	// console.log(z.length);
+// 	for (let i = 0; i < z.length; i++) {
+// 		if (z[i] == "x") {
+// 			x++;
+// 		} else if (z[i] == "o") {
+// 			o++;
+// 		}
+// 	}
+// 	return `Huruf x: ${x} | Huruf o: ${o}`;
+// }
 
-console.log(xo("XXXYYYOOOO"));
+// console.log(xo("XXXYYYOOOO"));
 
 // let a = "abc";
 // let b = "defd";
@@ -39,3 +39,51 @@ console.log(xo("XXXYYYOOOO"));
 // 	return str.repeat(num);
 // }
 // console.log(repeat("buah", 4));
+
+// function repeat(str, num) {
+// 	let string = "";
+// 	for (let i = 0; i < num; i++) {
+// 		string += str;
+// 	}
+// 	return string;
+// }
+
+// console.log(repeat("makan", 5));
+
+// function firstLast(str) {
+// 	let lastidx = str.length - 1;
+// 	console.log(str);
+// 	return `Huruf pertama: ${str[0]} | Huruf terakhir: ${str[lastidx]}`;
+// }
+// console.log(firstLast("Bambang"));
+// str = str.split("");
+// str.splice(0, 1);
+// str.splice(str.length - 1, 1);
+// return str.join("");
+// function removeFirstLast(str) {
+// 	str = str.split("");
+// 	str.shift();
+// 	str.pop();
+// 	return str.join("");
+// }
+// console.log(removeFirstLast("Adhi"));
+// console.log(removeFirstLast("Bambang"));
+// console.log(removeFirstLast("ABC"));
+
+function questionMark(str) {
+	let q = 0;
+	str = str.split("");
+	for (let index = 0; index < str.length; index++) {
+		if (str[index] == "?") {
+			q++;
+		}
+	}
+	if (q > 0) {
+		return q;
+	} else {
+		return "Tidak ada tanda tanya";
+	}
+}
+console.log(questionMark("??????asdasda"));
+console.log(questionMark("asdadsa"));
+console.log(questionMark("??"));
